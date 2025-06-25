47c0115f41b99abda69c28a8fea27553593059c8
atsushi-abuku <atsushijump623@gmail.com>
2025-06-25 22:26:37 +0900

    スクリプト追加
    基本的なスクリプトの追加

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //移動
+        Dash();
+        MoveRight();
+        MoveLeft();
+        Jump();
+    }
+
+    void MoveRight()
+    {
+        //Dキーを押すと右に動く
+
+    }
+    void MoveLeft()
+    {
+        //Aキーを押すと左に動く
+
+    }
+    void Jump()
+    {
+        //Wキーでジャンプ
+
+    }
+
+    void Dash()
+    {
+
+    }
+
+    //変装
+    void Disguise()
+    {
+
+    }
+}
