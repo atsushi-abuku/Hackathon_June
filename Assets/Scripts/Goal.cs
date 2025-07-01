c594e57bc593eb9649120a044972e948d786804c
atsushi-abuku <atsushijump623@gmail.com>
2025-07-01 21:47:14 +0900

    GoalとStageを変更

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,16 +1,25 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public bool isGoaled;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        isGoaled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //接触したら呼び出される
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("GOAL");
+        //isGoaledをtrueに変える
+
     }
 }
--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -2,15 +2,23 @@
 
 public class Stage : MonoBehaviour
 {
+    float time;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        time = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameOver();
+        //時間を減らす
     }
+
+    void GameOver()
+    {
+        //GAMEOVERなら「GAMEOVER」と表示
+    }
+
 }
