14e0d2989055b5ad0216b14e989f1aae2172f94b
amattakarinntou <snkarin@nike.eonet.ne.jp>
2025-07-08 00:17:19 +0900

    時間制限・GAMEOVERを追加
    Stageに時間制限(秒)のコードを追加。
    GAMEOVERがDbug.Logに表示。

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -3,22 +3,36 @@
 public class Stage : MonoBehaviour
 {
     float time;
+    float timeAccumulator = 0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        time = 10;
+        time = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
         GameOver();
-        //時間を減らす
+        //時間を減らす(1秒ごと）
+        timeAccumulator += Time.deltaTime;
+        if(timeAccumulator >= 1f)
+        {
+            time--;
+            timeAccumulator = 0f;
+        }
+        GameOver();
     }
 
     void GameOver()
     {
         //GAMEOVERなら「GAMEOVER」と表示
+        if (time == 0f)
+        {
+            Debug.Log("「GAMEOVER」");
+        }
     }
 
 }
