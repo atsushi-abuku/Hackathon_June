4e10c2710975c98aa18d38bbe261766749ff7c14
atsushi-abuku <atsushijump623@gmail.com>
2025-07-08 12:38:18 +0900

    stage.csを変更

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -1,12 +1,26 @@
 ﻿using UnityEngine;
 
+enum Fase
+{
+    GOING,
+    BACK,
+    FINISH
+}
 public class Stage : MonoBehaviour
 {
+    Fase fase;
     float time;
+
+    public GameObject field;
+    private Goal firstGoal;//行きのゴール
+    private Goal secondGoal;//帰りのゴール
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        fase = Fase.GOING;
         time = 10;
+        //field内のゴールをfirstGoal,secondGoalに代入
+
     }
 
     // Update is called once per frame
@@ -19,6 +33,27 @@ public class Stage : MonoBehaviour
     void GameOver()
     {
         //GAMEOVERなら「GAMEOVER」と表示
+    }
+
+    void CheckGoal()
+    {
+        switch (fase)
+        {
+            case Fase.GOING:
+                if (firstGoal.GetIsGoaled())
+                {
+                    //firstGoalを消し，secondGoalを出現させる
+
+                    fase = Fase.BACK;
+                }
+                break;
+            case Fase.BACK:
+                if (secondGoal.GetIsGoaled())
+                {
+                    fase = Fase.FINISH;
+                }
+                break;
+        }
     }
 
 }
